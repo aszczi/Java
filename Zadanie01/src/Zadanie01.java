4799dd76843664fa31e0a22fe6156c75c5c43cdd
aszczi <119359297+aszczi@users.noreply.github.com>
2025-03-04 19:35:58 +0100

    Add files via upload

---
+++ Zadanie01/src/Zadanie01.java
@@ -0,0 +1,18 @@
+class Zadanie01 {
+    private static final int BLAD = -2_000_000_000;
+
+    public Zadanie01() {
+    }
+
+    public void input( int bit ) {
+    }
+
+    public int wynik( ) {
+
+        return 0;
+    }
+
+    static void main( String[] args ) {
+        Zadanie01 z = new Zadanie01();
+    }
+}
